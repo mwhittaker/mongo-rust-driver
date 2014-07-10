eafc2188219e4b86cd553f4dc5f816b48453cf44
Xander Masotto <xander.masotto@10gen.com>
2014-07-10 15:38:03 -0700

    got it working

--- bson/src/encode.rs
+++ bson/src/encode.rs
@@ -1,14 +1,19 @@
 use std::ptr;
+use std::mem;
 use std::io::BufReader;
+use std::slice::raw::buf_as_slice;
+use std::raw::Slice;
+
+use super::bson_t;
+use super::bson_get_data;
 
 //fn bson_to_doc(bson: *const bson_t) {
-pub fn decode(bson: *mut bson_t) {
+pub fn decode(bson: *const super::bson_t) {
     unsafe {
-        let ptr: *const u8 = bson_data(bson);
+        let ptr: *const u8 = super::bson_get_data(bson);
         let n = Int::from_le(ptr::read(ptr as *const i32)) as uint;
         let buf: &[u8] =
-            std::mem::transmute(
-                std::raw::Slice { data: ptr.offset(4), len: n });
+            mem::transmute(Slice { data: ptr.offset(4), len: n });
         let mut reader = BufReader::new(buf);
 
         let result = match reader.read_byte() {
